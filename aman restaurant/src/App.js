942e68bec91532732d881c31af274d9705db6816
nuaman-choudhury6 <nuamanchy66@gmail.com>
2024-03-23 04:20:30 -0700

    first commit

---
+++ aman restaurant/src/App.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import Header from "./components/Layout/Headers";
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+}
+
+export default App;
